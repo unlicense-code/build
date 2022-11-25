02ab7279e8c8a5dcae0f7f354066e1d613cb8095
Frank Lemanschik <frank-dspeed@lemanschik.com>
2022-11-25 09:25:17 +0100

    Create os.js

---
+++ artifact/modules/os.js
@@ -0,0 +1,4 @@
+/**
+ * Rollup based Artifact build of the OS.JS Project for later migration. it is not worth 
+ * to create a source build for it maybe as updates will be only hyperapp elements.
+ */
