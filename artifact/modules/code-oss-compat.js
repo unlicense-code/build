5da9abbedbe840532668c7262b2ee255d36ff23b
Frank Lemanschik <frank-dspeed@lemanschik.com>
2022-11-25 09:27:31 +0100

    Create code-oss-compat.js

---
+++ artifact/modules/code-oss-compat.js
@@ -0,0 +1,5 @@
+/**
+ * Artifact Build for a code-oss-compat Module which offers 
+ * easy access and connection abstractions to code-oss based
+ * ides like vscode or theia or others. Also offers extension host
+ */
