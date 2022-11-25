937d31a7b8ba87949dcbb616d3704ec204d1f73d
Frank Lemanschik <frank-dspeed@lemanschik.com>
2022-11-25 09:23:18 +0100

    Create rollup.js

---
+++ artifact/modules/rollup.js
@@ -0,0 +1,6 @@
+/**
+ * Designed to build rollup from existing artifacts.
+ * Produces ESM Reuseable Rollup Build as also some
+ * Major Plugins and exposes that as dirEntrys 
+ * to get loaded in the target Environment.
+ */ 
